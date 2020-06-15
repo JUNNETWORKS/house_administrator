2c0f77b55f27f4f11917b81f3694fb25d7ff0527
Junichi Tanaka <junichi200123@gmail.com>
2020-06-16 05:52:41 +0900

    HTMLを返すようにするぞい

--- src/main.go
+++ src/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -10,28 +9,8 @@ import (
 func main() {
 	r := gin.Default()
 
-	/*
-		フロントエンド
-	*/
-	// htmlのディレクトリを指定
-	r.LoadHTMLGlob("templates/*")
-	r.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.html", gin.H{
-			"message": "This is index page",
-		})
-	})
-
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-
-	/*
-		バックエンド
-	*/
 	// 利用可能な部屋を返す
-	r.POST("/api", func(c *gin.Context) {
+	r.POST("/rooms", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"api_list": []string{
 				"jun_room",
@@ -39,17 +18,24 @@ func main() {
 			},
 		})
 	})
-	// 利用可能な操作を返す (エアコンや照明操作)
-	r.GET("/api/:room/", func(c *gin.Context) {})
-	// エアコンに対して何かしらの操作を送信するAPI
-	r.POST("/api/:room/air-conditioner", func(c *gin.Context) {
+	// 部屋のトップページを返す
+	r.GET("/rooms/:room/", func(c *gin.Context) {})
+	// 利用可能な操作を返す
+	r.GET("/rooms/:room/controllers", func(c *gin.Context) {})
+	// エアコンの操作に関する情報を返す (操作出来る項目や値の範囲など)
+	r.GET("/rooms/:room/controllers/air-conditioner", func(c *gin.Context) {
 		roomName := c.Param("room")
 		fmt.Printf("%s", roomName)
 	})
-	// 利用可能な値の種類を返す (温度や湿度,CO2濃度など)
-	r.GET("/api/:room/metrics", func(c *gin.Context) {})
-	// 部屋の温度を取得するAPI
-	r.GET("/api/:room/metrics/:", func(c *gin.Context) {})
+	// エアコンに対して何かしらの操作を送信するAPI
+	r.POST("/rooms/:room/controllers/air-conditioner", func(c *gin.Context) {})
+	// 将来的には以下の行の形式に置き換わる予定
+	r.GET("/rooms/:room/controllers/*device", func(c *gin.Context) {})
+	r.POST("/rooms/:room/controllers/*device", func(c *gin.Context) {})
+	// 利用可能な計測値の種類を返す (温度や湿度,CO2濃度など)
+	r.GET("/rooms/:room/measurements", func(c *gin.Context) {})
+	// 部屋の温度の詳細やグラフを表示するページを返す
+	r.GET("/rooms/:room/measurements/temp", func(c *gin.Context) {})
 
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
