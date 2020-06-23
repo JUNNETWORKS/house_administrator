d9165a50111fb40fade88c5a2965b6a6b498e653
Junichi Tanaka <junichi200123@gmail.com>
2020-06-24 00:54:09 +0900

    生成されたコードのインターフェースを満たすような MeasurementRecorderServer.Get 関数を作成した!!
    gRPCすごい! Go 楽しい
    
    そして, Goほぼほぼ忘れてるという...

--- src/services/measurement.go
+++ src/services/measurement.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"context"
+	"time"
 
 	pb "./pb"
 	"github.com/golang/protobuf/ptypes/empty"
@@ -10,13 +11,34 @@ import (
 type MeasurementRecorderServer struct {
 }
 
-func (server *MeasurementRecorderServer) Get(context.Context, *pb.MeasurementRequest) (*pb.MeasurementResponse, error) {
-	measurement = pb.Measurement{
-		RoomNumber: 
-		Type: pb.MeasurementType_TEMPERATURE
+// Get はセンサーから値を取得し, クライアントにMeasurementResponseオブジェクト返す関数です.
+func (server *MeasurementRecorderServer) Get(_ context.Context, message *pb.MeasurementRequest) (*pb.MeasurementResponse, error) {
+	var value float32
+	switch message.Type {
+	case pb.MeasurementType_TEMPERATURE:
+		value = 10.5 // ℃
+	case pb.MeasurementType_HUMIDITY:
+		value = 92 // %
+	case pb.MeasurementType_AIR_PRESSURE:
+		value = 1030 // hPa
+	case pb.MeasurementType_CO2_CONCENTRATION:
+		value = 60 // %
 	}
-	response = &pb.MeasurementResponse
+	currentTime := time.Now().Unix()
+	measurement := pb.Measurement{
+		RoomNumber: pb.RoomNumbers_JUN_ROOM,
+		Type:       pb.MeasurementType_TEMPERATURE,
+		Value:      value,
+		Datetime:   currentTime,
+	}
+	response := &pb.MeasurementResponse{
+		Measurement: &measurement,
+		Message:     "Succeeded get temperature value",
+		Error:       false,
+	}
+	return response, nil
 }
+
 func (server *MeasurementRecorderServer) GetAll(*empty.Empty, pb.MeasurementRecorder_GetAllServer) error {
 
 }
