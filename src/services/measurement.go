25c5882542dc9e966611d503098861c15e3abb5c
Junichi Tanaka <junichi200123@gmail.com>
2020-06-24 00:29:25 +0900

    コード生成した

--- src/services/measurement.go
+++ src/services/measurement.go
@@ -11,7 +11,11 @@ type MeasurementRecorderServer struct {
 }
 
 func (server *MeasurementRecorderServer) Get(context.Context, *pb.MeasurementRequest) (*pb.MeasurementResponse, error) {
-
+	measurement = pb.Measurement{
+		RoomNumber: 
+		Type: pb.MeasurementType_TEMPERATURE
+	}
+	response = &pb.MeasurementResponse
 }
 func (server *MeasurementRecorderServer) GetAll(*empty.Empty, pb.MeasurementRecorder_GetAllServer) error {
 
