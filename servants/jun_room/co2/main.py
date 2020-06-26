3211c8abbfe31cd029e8acbc0e5a478842b02103
Junichi Tanaka <junichi200123@gmail.com>
2020-06-27 07:08:57 +0900

    use_uart() コンテキストマネージャーを使うようにした

--- servants/jun_room/co2/main.py
+++ servants/jun_room/co2/main.py
@@ -1,16 +1,8 @@
-import subprocess
-
 import serial
-
-# モデルによってUARTに対する挙動が違う
-# ラズパイ zero W は以下の設定でOK
-# https://qiita.com/UedaTakeyuki/items/c5226960a7328155635f#%E4%BA%8B%E5%89%8D%E6%BA%96%E5%82%99uart%E3%82%92%E7%A9%BA%E3%81%91%E3%82%8B
-serial_dev = '/dev/ttyS0'
-stop_getty = 'sudo systemctl stop serial-getty@ttyS0.service'
-start_getty = 'sudo systemctl start serial-getty@ttyS0.service'
+from uart import use_uart
 
 
-def read():
+def read(serial_dev):
     """CO2センサーからデータを取得する関数
     https://toranoana-lab.hatenablog.com/entry/2019/12/27/180048
     """
@@ -23,7 +15,7 @@ def read():
                         timeout=1.0)
 
     # CO2濃度を取得するコマンド送信
-    result = ser.write(b'\xff\x01\x86\x00\x00\x00\x00\x00\x79')
+    _ = ser.write(b'\xff\x01\x86\x00\x00\x00\x00\x00\x79')
     s = ser.read(9)
 
     # 取得した形式が正しければCO2濃度を10進法に変換してretrun
@@ -32,6 +24,5 @@ def read():
 
 
 if __name__ == "__main__":
-    subprocess.call(stop_getty, stdout=subprocess.PIPE, shell=True)
-    print(f"CO2 = {read()}[ppm]")
-    subprocess.call(start_getty, stdout=subprocess.PIPE, shell=True)
+    with use_uart() as serial_dev:
+        print(f"CO2 = {read(serial_dev)}[ppm]")
