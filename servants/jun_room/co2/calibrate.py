ca12b98b87346f3c0118feb84352f8acfc5f0d8e
Junichi Tanaka <junichi200123@gmail.com>
2020-06-27 07:26:09 +0900

    calibrate.pyでもuse_uart()を使うようにした

--- servants/jun_room/co2/calibrate.py
+++ servants/jun_room/co2/calibrate.py
@@ -2,29 +2,22 @@
 # refer http://eleparts.co.kr/data/design/product_file/SENSOR/gas/MH-Z19_CO2%20Manual%20V2.pdf
 #
 # <C2><A9> Takeyuki UEDA 2015 -
-
-import subprocess
-import time
-
 import serial
 
-serial_dev = '/dev/ttyS0'
-stop_getty = 'sudo systemctl stop serial-getty@ttyS0.service'
-start_getty = 'sudo systemctl start serial-getty@ttyS0.service'
+from .uart import use_uart
 
 
-def calibrateZeroPoing():
+def calibrateZeroPoing(serial_dev):
     ser = serial.Serial(serial_dev,
                         baudrate=9600,
                         bytesize=serial.EIGHTBITS,
                         parity=serial.PARITY_NONE,
                         stopbits=serial.STOPBITS_ONE,
                         timeout=1.0)
-    result = ser.write(b"\xff\x01\x87\x00\x00\x00\x00\x00\x78")
+    _ = ser.write(b"\xff\x01\x87\x00\x00\x00\x00\x00\x78")
 
 
 if __name__ == '__main__':
-    subprocess.call(stop_getty, stdout=subprocess.PIPE, shell=True)
-    calibrateZeroPoing()
-    subprocess.call(start_getty, stdout=subprocess.PIPE, shell=True)
+    with use_uart() as serial_dev:
+        calibrateZeroPoing(serial_dev)
     print("caribration zero point done.")
