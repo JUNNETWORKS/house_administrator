fda08ec6575d8e75e6118c1fe47e0de9dd138933
Junichi Tanaka <junichi200123@gmail.com>
2020-06-28 12:05:01 +0900

    send_measurement() を実装した.

---
+++ servants/jun_room/grpc_client.py
@@ -0,0 +1,21 @@
+import time
+
+import grpc
+
+from pb import measurement_pb2, measurement_pb2_grpc, room_pb2
+
+measurement_store_uri = "localhost:1234"
+
+measurement = measurement_pb2.Measurement(
+    room_number=room_pb2.JUN_ROOM,
+    type=measurement_pb2.CO2_CONCENTRATION,
+    value=440,
+    datetime=time.time()
+)
+
+
+def send_measurement(measurement):
+    with grpc.insecure_channel(measurement_store_uri) as channel:
+        stub = measurement_pb2_grpc.MeasurementStoreStub(channel)
+        res = stub.InsertData(measurement)
+        print(res.message)
