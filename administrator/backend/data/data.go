0555f9712679825f03f31a92dfab3648b9dba511
Junichi Tanaka <junichi200123@gmail.com>
2020-08-01 07:22:59 +0900

    環境変数から値を取得して使う

--- administrator/backend/data/data.go
+++ administrator/backend/data/data.go
@@ -1,7 +1,9 @@
 package data
 
 import (
+	"github.com/JUNNETWORKS/house_administrator/utils"
 	"log"
+	"os"
 
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
@@ -13,7 +15,14 @@ var Db *sqlx.DB
 func init() {
 	var err error
 	// TODO: DBの名前は変えるかも
-	Db, err = sqlx.Open("postgres", "dbname=house sslmode=disable")
+	if dbport, ok := os.LookupEnv("DB_PORT"); !ok {
+		dbport = "5432"
+	}
+	if dbname, ok := os.LookupEnv("DB_NAME"); !ok {
+		dbname = "house"
+	}
+	dataSourceName := fmt.Sprintf("port=%s dbname=%s sslmode=disable", dbport, dbname)
+	Db, err = sqlx.Open("postgres", dataSourceName)
 	if err != nil {
 		log.Fatal(err)
 	}
