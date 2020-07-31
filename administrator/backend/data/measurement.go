abc6597d3e48e42ee394f93ba2e605f67f2b92d2
Junichi Tanaka <junichi200123@gmail.com>
2020-08-01 06:29:12 +0900

    add: Measurements()

--- administrator/backend/data/measurement.go
+++ administrator/backend/data/measurement.go
@@ -1,6 +1,10 @@
 package data
 
-import "time"
+import (
+	"github.com/jmoiron/sqlx"
+	"log"
+	"time"
+)
 
 type Measurement struct {
 	Id        int
@@ -8,4 +12,21 @@ type Measurement struct {
 	Value     float64
 	CreatedAt time.Time
 	UpdatedAt time.Time
+}
+
+func Measurements() (measurements []Measurements, err error) {
+	rows, err := Db.Queryx("SELECT * FROM measurements")
+	if err != nil {
+		log.Println("Failed to get all measurements")
+		return
+	}
+	for rows.Next() {
+		var measurement Measurement
+		err = rows.StructScan(&measurement)
+		if err != nil {
+			return
+		}
+		measurements = append(measurements, measurement)
+	}
+	return
 }
