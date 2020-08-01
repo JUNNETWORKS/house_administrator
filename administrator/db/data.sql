575f3bba5b0fac6365d477c5b10b8e85ab28acce
Junichi Tanaka <junichi200123@gmail.com>
2020-08-01 11:48:50 +0900

    テストようにデータをいくつか挿入するSQLを書いた

---
+++ administrator/db/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO users
+    (id, name, created_at, updated_at)
+VALUES
+    (1, 'JUN', '1999-01-08 04:05:06', '1999-01-08 04:05:06');
+
+INSERT INTO rooms
+    (name, description, owner_id, created_at, updated_at)
+VALUES('JUN Room', 'this is test room', 1, '2000-01-01 00:00:00', '2000-01-01 00:00:00');
