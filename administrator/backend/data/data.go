23ac2d363a7cebedd64ff384352e51b802dcce22
Junichi Tanaka <junichi200123@gmail.com>
2020-08-01 11:33:16 +0900

    環境変数からDBのユーザーだのパスワードだのを取得する
    そんでDB接続で使う

--- administrator/backend/data/data.go
+++ administrator/backend/data/data.go
@@ -9,17 +9,18 @@ import (
 	_ "github.com/lib/pq"
 )
 
-// sqlにアクセスするためのパッケージ変数
+// Db はsqlにアクセスするためのパッケージ変数
 var Db *sqlx.DB
 
 func init() {
 	var err error
 	// TODO: DBの名前は変えるかも
-	var dbhost, dbport, dbname string
-	dbhost = utils.GetEnvWithDefault("DB_HOST", "db")
-	dbport = utils.GetEnvWithDefault("DB_PORT", "5432")
-	dbname = utils.GetEnvWithDefault("DB_NAME", "house")
-	dataSourceName := fmt.Sprintf("host=%s port=%s dbname=%s sslmode=disable", dbhost, dbport, dbname)
+	dbhost := utils.GetEnvWithDefault("DB_HOST", "db")
+	dbport := utils.GetEnvWithDefault("DB_PORT", "5432")
+	dbname := utils.GetEnvWithDefault("DB_NAME", "house")
+	dbuser := utils.GetEnvWithDefault("DB_USER", "postgres")
+	dbpass := utils.GetEnvWithDefault("DB_PASS", "postgres")
+	dataSourceName := fmt.Sprintf("host=%s port=%s dbname=%s user=%s password=%s sslmode=disable", dbhost, dbport, dbname, dbuser, dbpass)
 	Db, err = sqlx.Open("postgres", dataSourceName)
 	if err != nil {
 		log.Fatal(err)
