65ef4a5375f456e5e255113464ccacdbf9e7dc6c
Junichi Tanaka <junichi200123@gmail.com>
2020-08-03 01:34:28 +0900

    user.go 内の構造体と関数にコメントを追加

--- administrator/backend/data/user.go
+++ administrator/backend/data/user.go
@@ -2,6 +2,7 @@ package data
 
 import "time"
 
+// User ... ユーザーを表す構造体
 type User struct {
 	ID        int
 	Name      string
@@ -9,6 +10,7 @@ type User struct {
 	UpdatedAt time.Time
 }
 
+// Users ... 全ユーザーを取得する関数
 func Users() ([]User, error) {
 	var users []User
 	err := Db.Select(&users, "SELECT * FROM users")
