67e12cbac03bc0e52ab3be8ba288204b6016791d
Junichi Tanaka <junichi200123@gmail.com>
2020-08-04 08:49:36 +0900

    最高司祭様を初期データに追加

--- administrator/db/data.sql
+++ administrator/db/data.sql
@@ -1,6 +1,7 @@
 INSERT INTO users
     (id, name, created_at, updated_at)
 VALUES
+    (0, 'Administrator', '1900-01-01 00:00:00', '1900-01-01 00:00:00'),
     (1, 'JUN', '1999-01-08 04:05:06', '1999-01-08 04:05:06');
 
 INSERT INTO rooms
