fa6f696f610d289f48e6edd29eb69f7cf2e683ff
Junichi Tanaka <junichi200123@gmail.com>
2020-08-04 08:34:38 +0900

    GetRooms() の説明コメントを追加

--- administrator/backend/handlers/room.go
+++ administrator/backend/handlers/room.go
@@ -9,6 +9,14 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
+// GetRooms ... 全ての部屋の情報をJSONで返す
+// レスポンスの例
+// [{'id': 1,
+//   'name': 'JUN Room',
+//   'description': 'this is test room',
+//   'owner_id': 1,
+//   'created_at': '2000-01-01T00:00:00Z',
+//   'updated_at': '2000-01-01T00:00:00Z'}]
 func GetRooms(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	rooms, err := data.GetRooms()
 	if err != nil {
