f97472782ef22ec56013601081213bbb3ca08f80
Junichi Tanaka <junichi200123@gmail.com>
2020-08-04 07:06:16 +0900

    handlers.GetRooms() を実装

--- administrator/backend/handlers/room.go
+++ administrator/backend/handlers/room.go
@@ -1,1 +1,27 @@
 package handlers
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/JUNNETWORKS/house_administrator/data"
+	"github.com/julienschmidt/httprouter"
+)
+
+func GetRooms(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	rooms, err := data.GetRooms()
+	if err != nil {
+		log.Println(err)
+	}
+	output, err := json.Marshal(&rooms)
+	if err != nil {
+		log.Println(err)
+	}
+	w.Write(output)
+	return
+}
+
+func AddRoom(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	return
+}
