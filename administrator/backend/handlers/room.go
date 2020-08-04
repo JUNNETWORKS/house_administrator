8b36a0f6850a3d270425d33ec4e215be90c76daa
Junichi Tanaka <junichi200123@gmail.com>
2020-08-05 01:45:11 +0900

    RegisterRoomハンドラー完成〜

--- administrator/backend/handlers/room.go
+++ administrator/backend/handlers/room.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 
@@ -27,9 +28,41 @@ func GetRooms(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 		log.Println(err)
 	}
 	w.Write(output)
-	return
 }
 
-func RegisterRoom(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	return
+// RegisterRoom ... 新しい部屋を登録する
+// リクエストの例
+// {
+// 	"name": "New Room",
+// 	"description": "This is new room"
+// 	"owner_id": 1
+// }
+func RegisterRoom(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	len := r.ContentLength
+	body := make([]byte, len)
+	_, err := r.Body.Read(body)
+	if err != nil && err.Error() != "EOF" {
+		log.Printf("%s", string(body))
+		log.Println("リクエストが間違っています.")
+		log.Println(err.Error())
+		w.Write([]byte(err.Error()))
+		return
+	}
+	var room *data.Room
+	err = json.Unmarshal(body, &room)
+	if err != nil {
+		log.Println("リクエストボディから構造体にマッピング出来ませんでした.")
+		log.Println(err.Error())
+		w.Write([]byte(err.Error()))
+		return
+	}
+	err = room.Create()
+	if err != nil {
+		log.Println("新しい部屋をDBに挿入出来ませんでした.")
+		log.Println(err.Error())
+		w.Write([]byte(err.Error()))
+		return
+	}
+	log.Printf("新しい部屋を追加しました\n%v\n", room)
+	fmt.Fprintf(w, "%s: %v", "新しい部屋", room)
 }
