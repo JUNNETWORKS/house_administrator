a0782b9be87241339e83afe2c45b68b09795f3ad
Junichi Tanaka <junichi200123@gmail.com>
2020-11-28 10:37:10 +0900

    feat(front): add roomItem
    まだpropsとかは設定してない状態

---
+++ administrator/front/src/components/roomItem.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import Link from 'next/link';
+
+const RoomItem: React.FC = () => {
+  return (
+    <>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          border: '1px solid black',
+        }}
+      >
+        <img
+          src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.thespruce.com%2Fliving-room-lighting-ideas-4134256&psig=AOvVaw2kvz9OKm-sW3NR76ccLkuv&ust=1606579346393000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLC_082Mo-0CFQAAAAAdAAAAABAD"
+          alt="Room Picture"
+          width={256}
+          height={144}
+        />
+        <p>Room Name</p>
+        <p>Room Description</p>
+        <Link href="/rooms/hogehoge">
+          <a>Go to Room Page</a>
+        </Link>
+      </div>
+    </>
+  );
+};
+
+export default RoomItem;
